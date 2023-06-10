fdff8de5b1724b562510017856b4b3e547a4110d
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-06-10 08:14:18 +0100

    feat(profile): fetch user informations

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,36 @@
-export default async function Profile() {
-    return <h1>Hello, Next.js!</h1>
+"use client";
+
+import {useRouter} from "next/navigation";
+import {useEffect, useState} from "react";
+import {User} from "@/classes/User";
+import Link from "next/link";
+import {inspect} from "util";
+import styles = module
+
+function Profile() {
+
+    const router = useRouter();
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        async function fetchUser() {
+            const token =  await User.getToken();
+            const user = await User.getUserInformations(token);
+            setUser(user);
+        }
+
+        fetchUser();
+    }, []);
+
+    return (
+        <div>
+            <p>This page is for profile</p>
+            <Link href={'/feed'}>
+                <p style={styles.underline}>Navigate to feed</p>
+            </Link>
+        </div>
+    );
 }
+
+export default Profile;
