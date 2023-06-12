e184c9bbb79c696d9d2976e49614c59043ececc1
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-06-12 15:20:51 +0100

    feat(profile): finish html css design of the desktop profile page

--- src/app/components/profile/desktop/DesktopProfile.tsx
+++ src/app/components/profile/desktop/DesktopProfile.tsx
@@ -6,6 +6,7 @@ import {useEffect, useState} from "react";
 import {User} from "@/classes/User";
 import DesktopHeader from "@/app/components/profile/desktop/DesktopHeader";
 import DesktopNav from "@/app/components/profile/desktop/DesktopNav";
+import Image from "next/image";
 
 export default function DesktopProfile(){
     const [user, setUser] = useState(null);
@@ -25,7 +26,32 @@ export default function DesktopProfile(){
             <div className={styles.container}>
                 <div className={styles.userInformations}>
 
+                        <div className={styles.imageContainer}>
+                            <Image src={"/profil-de-lutilisateur.png"} alt={"profile picture"} width={250} height={250} className={styles.profileImage}></Image>
+                            <p>Change Profile Picture</p>
+                        </div>
+
+                        <div className={styles.userStats}>
+                            <p>UserName</p>
+                            <div className={styles.stats}>
+                                <p>stat 1</p>
+                                <p>stats 2</p>
+                            </div>
+                        </div>
+                        <button type="button" className={styles.createPlaylist}>Create a Playlist</button>
+
+
                 </div>
+                <div className={styles.profileLinks}>
+                    <p>Link 1</p>
+                    <p>Link 2</p>
+                    <p>Link 3</p>
+                    <p>Link 4</p>
+                    <p>Link 5</p>
+                </div>
+
+
+
                 <div className={styles.playlistContainer}>
                     <h2 className={styles.subtitle}>My Playlists</h2>
                 </div>
@@ -33,9 +59,6 @@ export default function DesktopProfile(){
                     <h2 className={styles.subtitle}>Recently followed</h2>
                 </div>
             </div>
-
-
-
         </div>
     )
 }
